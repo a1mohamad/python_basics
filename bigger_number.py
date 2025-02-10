602071765ab189dd272bdcfedd34fae0ef774474
a1mohamad <a1.mohamad.askari@gmail.com>
2025-02-10 21:52:30 +0330

    add bigger_number.py

---
+++ bigger_number.py
@@ -0,0 +1,8 @@
+adade_aval = int (input())
+adade_dovom = int (input())
+if adade_aval > adade_dovom :
+    print  (adade_aval)
+elif adade_aval < adade_dovom :
+    print (adade_dovom)
+elif adade_aval == adade_dovom :
+    print (adade_aval)             
