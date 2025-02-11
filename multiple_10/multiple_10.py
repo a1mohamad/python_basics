ecbc79859ac19da3f2c37769659a7d393ac0fef7
a1mohamad <a1.mohamad.askari@gmail.com>
2025-02-11 19:12:54 +0330

    add multiple_10 file

---
+++ multiple_10/multiple_10.py
@@ -0,0 +1,5 @@
+number = int (input())
+if number % 10 == 0 :
+    print (number + 10)
+else :
+    print (((number // 10) + 1) * 10)
---
+++ multiple_10/readme.txt
@@ -0,0 +1,1 @@
+In this exercise, we get a number and give next multiple of 10 of this number.
