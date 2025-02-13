7f977da08ab663b0c8715d015db8cc267b041442
a1mohamad <a1.mohamad.askari@gmail.com>
2025-02-13 19:36:04 +0330

    add age category exercise

---
+++ age_category/age_category.py
@@ -0,0 +1,13 @@
+age = int (input())
+if 0 < age < 6 :
+    print ('baby')
+elif 6 <= age < 10 :
+    print ('child')
+elif 10 <= age < 14 :
+    print ('teenager')
+elif 14 <= age < 24 :
+    print ('adult')
+elif 24 <= age < 40 :
+    print ('young')
+elif 40 <= age :
+    print ('old')    
---
+++ age_category/readme.txt
@@ -0,0 +1,7 @@
+In this exercise, we receive our user age as input and tell the user the age category under this rule:
+0-6  :  baby
+6-10  :  child
+10-14  : teenager
+14-24  :  adult
+24-40  :  young
+40>  :  old
