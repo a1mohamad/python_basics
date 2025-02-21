4855468857fabb53ea11b63aa62ee58df8e07559
a1mohamad <a1.mohamad.askari@gmail.com>
2025-02-21 20:15:15 +0330

    add laptop compare exercise

---
+++ laptop_compare/laptop_compare.py
@@ -0,0 +1,20 @@
+
+n = int(input())
+my_input =[]
+sum_data = []
+for i in range(0 , n):
+    my_input =[int(item) for item in input().split()]
+    my_data = [my_input]
+    sum_data = sum_data + my_data
+
+
+classify = 0
+for i in range(0 , n):
+    for j in range(0 , n):
+        if sum_data[i][0] > sum_data[j][0] and sum_data[i][1] < sum_data[j][1] :
+            classify = 1
+
+if classify == 1 :
+    print('happy jennifer')
+elif classify == 0 :
+    print('poor jennifer')
